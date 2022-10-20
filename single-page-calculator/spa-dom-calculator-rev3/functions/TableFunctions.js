398298cab0f3bbf21fc791228bfeecb6ee84e7e7
ikiusen <thiagofcabrera00@gmail.com>
2022-10-20 18:59:38 -0300

    added license

--- single-page-calculator/spa-dom-calculator-rev3/functions/TableFunctions.js
+++ single-page-calculator/spa-dom-calculator-rev3/functions/TableFunctions.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2022 Thiago Cabrera All rights reserved.
+ * Contact: thiagofcabrera00@gmail.com
+ * Released under the GPLv3
+ * https://www.gnu.org/licenses/gpl-3.0
+ **/
 function setElementOnTable(table, row, column, element){
     table.childNodes[0].childNodes[row].childNodes[column].appendChild(element);
 }
--- single-page-calculator/spa-html-calculator/controller.js
+++ single-page-calculator/spa-html-calculator/controller.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2022 Thiago Cabrera All rights reserved.
+ * Contact: thiagofcabrera00@gmail.com
+ * Released under the GPLv3
+ * https://www.gnu.org/licenses/gpl-3.0
+ **/
 let btn0 = document.getElementById("btn0");
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
