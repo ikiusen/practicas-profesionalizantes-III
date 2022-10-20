c3c1e7a03e567ab25cefd4292b685935c5f02415
ikiusen <thiagofcabrera00@gmail.com>
2022-10-20 19:10:08 -0300

    added license

--- application-template/components/application/html-elements/navigation-bar/view/NavigationBar.js
+++ application-template/components/application/html-elements/navigation-bar/view/NavigationBar.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2022 Thiago Cabrera All rights reserved.
+ * Contact: thiagofcabrera00@gmail.com
+ * Released under the GPLv3
+ * https://www.gnu.org/licenses/gpl-3.0
+ **/
 import { DropdownList } from '../../dropdown-list/view/DropdownList.js';
 import { NavigationBarController } from '../controller/NavigationBarController.js';
 class NavigationBar extends HTMLElement {
--- application-template/components/modal/main.js
+++ application-template/components/modal/main.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2022 Thiago Cabrera All rights reserved.
+ * Contact: thiagofcabrera00@gmail.com
+ * Released under the GPLv3
+ * https://www.gnu.org/licenses/gpl-3.0
+ **/
 import { ModalDialog } from './view/ModalDialog.js';
 import { FormModel } from '../form/model/FormModel.js';
 import { FormView } from '../form/view/FormView.js';
